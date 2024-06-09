6c1a0cf1061cafb1cf9e8e57549eedad4ae2b1cb
vidhimeel <vidhimeel@gmail.com>
2024-06-09 12:30:44 +0530

    PerimeterAndAreaOfRectMiniApp

--- ProgrammingFundamentals/Ep2/PerimeterAndAreaOfRect/src/Task.kt
+++ ProgrammingFundamentals/Ep2/PerimeterAndAreaOfRect/src/Task.kt
@@ -1,11 +1,11 @@
 import com.faangx.ktp.basics.PerimeterAndAreaOfRectMiniApp
 
 fun getPerimeterOfRectangle(l: Int, b: Int): Int {
-    TODO()
+    return 2 * (l + b)
 }
 
 fun getAreaOfRectangle(l: Int, b: Int): Int {
-    TODO()
+     return l * b
 }
 
 fun main() {
