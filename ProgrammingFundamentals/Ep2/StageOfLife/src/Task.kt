74267d7e7cf22c3bb584d5b2cedafda66e838ace
vidhimeel <vidhimeel@gmail.com>
2024-06-10 22:38:23 +0530

    Stage Of Life

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,26 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    when {
+        age in 1..2 -> {
+            return "Infancy"
+        }
+        age in 3..11 -> {
+            return "Childhood"
+        }
+        age in 12..17 -> {
+            return "Teenage"
+        }
+        age in 18..59 -> {
+            return "Adulthood"
+        }
+        age >= 60 -> {
+            return "Old age"
+        }
+        else -> {
+            return ""
+        }
+    }
 }
 
 fun main() {
