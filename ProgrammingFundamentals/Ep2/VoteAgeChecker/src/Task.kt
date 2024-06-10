f44ca111e5b819c3b1cbd6f9c75074a7388a705e
vidhimeel <vidhimeel@gmail.com>
2024-06-10 21:54:54 +0530

    Vote Age Checker

--- ProgrammingFundamentals/Ep2/VoteAgeChecker/src/Task.kt
+++ ProgrammingFundamentals/Ep2/VoteAgeChecker/src/Task.kt
@@ -1,7 +1,11 @@
 import com.faangx.ktp.basics.VoteAgeCheckerMiniApp
 
 fun canVote(age: Int): Boolean {
-    TODO()
+    if (age >= 18) {
+        return true
+    }else {
+        return false
+    }
 }
 
 fun main() {
