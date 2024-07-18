875002dda1e632624c64b3a9c9a93d74409f9326
vidhimeel <vidhimeel@gmail.com>
2024-07-18 19:59:14 +0530

    Pattern PD3 Completed.

--- ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
@@ -1,7 +1,14 @@
 import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines){ i ->
+        repeat(i) { print(' ') }
+
+        repeat(2 * (lines - i) - 1) { j ->
+            if (j % 2 == 0) print(char1) else print(char2)
+        }
+        println()
+    }
 }
 
 fun main() {
