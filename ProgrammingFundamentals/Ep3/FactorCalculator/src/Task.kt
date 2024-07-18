2f8470be450bd68db78da7f017220e5cb94382ee
vidhimeel <vidhimeel@gmail.com>
2024-07-18 19:57:29 +0530

    factor calculator Completed.

--- ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
@@ -1,11 +1,22 @@
 import com.faangx.ktp.basics.FactorCalculatorMiniApp
 
 fun printFactorsOf(num: Int) {
-    TODO()
+    for (i in 1..num){
+        if (num % i == 0){
+            println("$i,")
+        }
+    }
 }
 
+
 fun isPrime(num: Int): Boolean {
-    TODO()
+    var noOfFactors = 0
+    for (i in 1..num){
+        if (num % i == 0){
+           noOfFactors += 1
+        }
+    }
+     return noOfFactors == 2
 }
 
 fun main() {
--- ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorialCalculator/src/Task.kt
@@ -1,15 +1,23 @@
 import com.faangx.ktp.basics.FactorialCalculatorMiniApp
 
 fun factorialOf(x: Long): Long {
-    TODO()
+    var factorial = 1L
+    for (i in 2L..x){
+        factorial *= i
+    }
+    return factorial
 }
 
 fun permutationsOf(n: Long, r: Long): Long {
-    TODO()
+    var permutations = 1L
+    for (i in (n - r + 1)..n) {
+        permutations *= 1
+    }
+    return permutations
 }
 
 fun combinationsOf(n: Long, r: Long): Long {
-    TODO()
+    return permutationsOf(n , r) / factorialOf(r)
 }
 
 fun main() {
