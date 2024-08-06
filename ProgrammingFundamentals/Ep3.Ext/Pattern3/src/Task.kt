27c74a283b26e310945df8b6e876198a7ae426b6
vidhimeel <vidhimeel@gmail.com>
2024-08-06 20:42:26 +0530

    Pattern3 completed.

--- ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern3/src/Task.kt
@@ -1,7 +1,31 @@
 import com.faangx.ktp.patterns.single.PatternLinesAndTwoCharsBasedMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+
+    var k = 0
+
+    for (i in 1..lines) {
+        for (j in 1..lines - i) {
+            print(char2)
+        }
+
+        print(char1)
+
+        while (k != 2 * i - 1) {
+            if (i != 1)
+                print(char2)
+            k++
+        }
+        if (i != 1)
+            print(char1)
+
+        for (j in 1 .. lines - i){
+            print(char2)}
+
+        k = 2
+        println()
+
+    }
 }
 
 fun main() {
