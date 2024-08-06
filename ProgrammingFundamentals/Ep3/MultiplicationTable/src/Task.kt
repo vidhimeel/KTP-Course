cf040168c2089fa25ab2255d74b07dba56bdec76
vidhimeel <vidhimeel@gmail.com>
2024-08-06 20:41:09 +0530

    Multiplication Table completed.

--- ProgrammingFundamentals/Ep3/MultiplicationTable/src/Task.kt
+++ ProgrammingFundamentals/Ep3/MultiplicationTable/src/Task.kt
@@ -2,7 +2,7 @@ import com.faangx.ktp.basics.MultiplicationTableMiniApp
 
 fun printTable(num: Int) {
     repeat(10){
-        println("$num X ${it + 1} = ${num * (it + 1)}")
+        //println("$num X $i = ${num * i}")
     }
 }
 
